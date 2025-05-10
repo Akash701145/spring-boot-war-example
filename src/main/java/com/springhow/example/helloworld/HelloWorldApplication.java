8d5303857d4cd055f2e608924586b760059e593a
Akash Tomer <140230413+Akash701145@users.noreply.github.com>
2025-05-11 04:33:49 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Akash Tomer!";
+        return "Hello World!";
     }
 
 }
